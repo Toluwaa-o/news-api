b220d2eafb7802788da17cfe8deab3ece9d2ced8
Toluwalashe <asheogunleye02@gmail.com>
2023-08-31 23:07:03 +0100

    middlewares

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ require("express-async-errors");
 
 const express = require("express");
 const morgan = require("morgan");
+const errorHandler = require("./middleware/error-handler");
+const notFound = require("./middleware/not-found");
 
 const NewsRoutes = require("./routes/GetNews");
 
 const app = express();
 
 app.use(morgan("tiny"));
+
+app.use(errorHandler);
+app.use(notFound);
 
 app.use("/my-news-api/v1/", NewsRoutes);
 const port = process.env.PORT || 3000;
