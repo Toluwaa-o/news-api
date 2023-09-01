c2a339c76be1251054c1f20e22b0bd3f6bc61739
Toluwalashe <asheogunleye02@gmail.com>
2023-09-01 01:37:50 +0100

    return arrray

--- controllers/GetNews.js
+++ controllers/GetNews.js
@@ -14,7 +14,7 @@ const getNews = async (req, res) => {
   const news = await axios.get(url);
   const result = news.data.articles.splice(+page - 1, 10);
 
-  return res.json({ result });
+  return res.json(result);
 };
 
 const getCountryHeadlines = async (req, res) => {
